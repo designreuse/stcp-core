8c532de1731bfc8b0901ef789b271538465a980b
jirapatj <jewel.jirapat@gmail.com>
2016-04-23 12:55:09 +0700

    #create course planner service

--- src/main/java/com/kmutt/stcp/service/CoursePlannerService.java
+++ src/main/java/com/kmutt/stcp/service/CoursePlannerService.java
@@ -1,5 +1,7 @@
 package com.kmutt.stcp.service;
 
+import com.kmutt.stcp.entity.Account;
+import com.kmutt.stcp.entity.CoursePlan;
 import com.kmutt.stcp.entity.Subject;
 import com.kmutt.stcp.repository.SubjectRepository;
 import org.slf4j.Logger;
@@ -7,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,5 +29,41 @@ public class CoursePlannerService {
 
     public List<Subject> getSubjectListTest() {
         return subjectRepository.findAll();
+    }
+
+    public CoursePlan getCoursePlan(Account account,int semester,int term) {
+
+        return new CoursePlan();
+    }
+
+
+    public List<CoursePlan> getCoursePlan(Account account) {
+
+        return new ArrayList<>();
+    }
+
+    public List<CoursePlan> getCoursePlan(Account account,int year) {
+
+        return new ArrayList<>();
+    }
+
+    public boolean insertCoursePlan(CoursePlan coursePlan) {
+
+        return false;
+    }
+
+    public Subject getSubject(String subjectId) {
+
+        return new Subject();
+    }
+
+    public List<Subject> getSubject(String code, String name) {
+
+        return new ArrayList<>();
+    }
+
+    public boolean deleteCoursePlan(int coursePlanId) {
+
+        return false;
     }
 }
